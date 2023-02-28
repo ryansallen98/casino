f3bd40e8e8b684d59bbbf3bc337fb3fe557a4a7b
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-02-28 16:21:12 +0000

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -217,6 +217,7 @@ async function postIpn(req, res) {
     ipn.recipientArray = recipientArray;
     ipn.ipAddress = ipAddress;
     // validate that transaction settles new order
+    console.log(ipn.paymentId)
     invoiceDB.find({ paymentId: ipn.paymentId }, function (err, docs) {
       if (err) {
         // Error message if the paymentID doesn't match
