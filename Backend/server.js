0ffd8102157922ad05a090199fa1ac339a0c1330
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-02-28 13:00:20 +0000

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,234 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const Datastore = require('nedb');
+const axios = require('axios')
+const uri = 'https://bux.digital/v1/pay/?';
+const app = express();
+
+
+const usersDB = new Datastore({ filename: './database/users.db', autoload: true });
+const invoiceDB = new Datastore({ filename: './database/invoice.db', autoload: true });
+const paidDB = new Datastore({ filename: './database/paid.db', autoload: true });
+
+// Serve static files from the frontend folder
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+app.post('/login', (req, res) => {
+  console.log(true)
+  // Get the email and password from the request body
+  const { username, password } = req.body;
+
+  // Find the user in the database
+  usersDB.findOne({ username, password }, (err, user) => {
+    if (err) {
+      // Return an error message if there's an error with the database
+      res.status(500).json({ error: 'Server error' });
+    } else if (!user) {
+      // Return an error message if the username or password is incorrect
+      res.status(401).json({ error: 'Invalid username or password' });
+    } else {
+      // Generate a JWT token xxwith a payload that includes the user's ID and username
+      const token = jwt.sign({ userId: user._id, username }, 'secret-key');
+
+      // Return the token as part of the response
+      res.json({ token });
+    }
+  });
+});
+
+// Route that handles sign-up requests
+app.post('/signup', (req, res) => {
+  // Get the username and password from the request body
+  const user = req.body;
+  user.mainBalance = 0;
+  user.bonusBalance = 0;
+  console.log(user)
+  const username = user.username;
+
+  // Check if the username and password are valid
+  // You can use a database to verify the user's credentials and prevent duplicate usernames
+  if (user.username && user.password) {
+    // Check if the username already exists in the database
+    usersDB.findOne({ username }, (err, data) => {
+      if (err) {
+        // Return an error message if there's an error with the database
+        res.status(500).json({ error: 'Server error' });
+      } else if (data) {
+        // Return an error message if the username already exists
+        res.status(400).json({ error: 'Username already exists' });
+      } else {
+        usersDB.insert(user, (err, user) => {
+          if (err) {
+            // Return an error message if there's an error with the database
+            res.status(500).json({ error: 'Server error' });
+          } else {
+            // Generate a JWT token with a payload that includes the user's ID and username
+            const token = jwt.sign({ userId: user._id, username }, 'secret-key');
+
+            // Return the token as part of the response
+            res.json({ token });
+          }
+        });
+      }
+    });
+  } else {
+    // Return an error message if the sign-up fails
+    res.status(400).json({ error: 'Invalid username or password' });
+  }
+});
+
+// Protected route that requires authentication
+app.get('/api/protected', (req, res) => {
+  // Get the token from the request headers
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  // Verify the token
+  jwt.verify(token, 'secret-key', (err, decoded) => {
+    if (err) {
+      // Return an error message if the token is invalid
+      res.status(401).json({ error: 'Invalid token' });
+    } else {
+      // Return some protected data if the token is valid
+      console.log(decoded.userId)
+      usersDB.findOne({ _id: decoded.userId }, (err, user) => {
+        if (err) {
+          // Return an error message if there's an error with the database
+          res.status(500).json({ error: 'Server error' });
+        } else if (!user) {
+          // Return an error message if the user is not found
+          res.status(404).json({ error: 'User not found' });
+        } else {
+          // Return the user's information as part of the response
+          console.log(user)
+          res.json({ user });
+        }
+      })
+    }
+  });
+});
+
+app.post('/deposit', async (req, res) => {
+  console.log(req.body)
+
+
+  const code = Math.random().toString(36).substring(7);
+  const invoiceId = Math.random().toString(36).substring(7);
+  const params = {
+    merchant_name: 'iCore Pay',
+    invoice: invoiceId,
+    order_key: code,
+    merchant_addr: 'etoken:qp468rr6gggl36hvz297krsjz0l3euq52cv4tpux4k',
+    amount: req.body.amount,
+    success_url: 'https://google.com',
+    cancel_url: 'https://google.com',
+    ipn_url: 'https://google.com',
+    return_json: true
+  };
+  console.log(params);
+
+  // create the invoice URI by appending the params to the base URI
+  // encode the key-value pairs of the params object as query parameters
+  const queryParams = Object.keys(params).map((key) => {
+    if (Array.isArray(params[key])) {
+      return `${key}=${encodeURIComponent(JSON.stringify(params[key]))}`;
+    }
+    return `${key}=${encodeURIComponent(params[key])}`;
+  }).join('&');
+  // append the query parameters to the URI
+  const getUrl = `${uri}${queryParams}`;
+  console.log('url', getUrl);
+
+  try {
+    const response = await axios.get(getUrl, { mode: 'no-cors' });
+    console.log(response.data);
+    response.data.user = req.body.user
+    invoiceDB.insert(response.data)
+    let payURL = response.data.paymentUrl;
+    res.json({ payURL });
+  } catch (error) {
+    console.log(error.code);
+  }
+})       
+
+
+async function postIpn(req, res) {
+  const ipAddress = req.connection.remoteAddress;
+  console.log(ipAddress);
+  const allowIps = [
+    '::ffff:208.113.133.143',
+    '::ffff:45.79.36.250',
+    '::ffff:127.0.0.1'
+  ]
+  let isTrue = 0
+  allowIps.map(ip => {
+    if (ip === ipAddress) {
+      isTrue++
+    }
+  })
+  if (isTrue === 0) {
+    console.log('error wrong IP Address')
+  } else {
+    const ipn = req.body;
+    console.log(ipn)
+    const url = `https://ecash.badger.cash:8332/tx/${ipn.txn_id}?slp=true`;
+    const result = await axios.get(url);
+    const txData = result.data;
+    const outputs = txData.outputs;
+    const buxTokenId = "7e7dacd72dcdb14e00a03dd3aff47f019ed51a6f1f4e4f532ae50692f62bc4e5";
+    const buxDecimals = 4;
+    const isBuxTransaction = txData.slpToken.tokenId === buxTokenId;
+    let recipientArray = [];
+    if (isBuxTransaction) {
+      for (let i = 1; i < outputs.length; i++) {
+        const isSlpOutput = outputs[i].slp;
+        if (isSlpOutput) {
+          const buxAmount = +(outputs[i].slp.value) / 10 ** buxDecimals;
+          recipientArray.push({
+            address: convertAddress(outputs[i].address, "etoken"),
+            buxAmount: buxAmount
+          });
+        }
+      }
+    }
+
+    // function returns address with desired prefix
+    function convertAddress(address, targetPrefix) {
+      const { prefix, type, hash } = ecashaddr.decode(address);
+      if (prefix === targetPrefix) {
+        return address;
+      } else {
+        const convertedAddress = ecashaddr.encode(targetPrefix, type, hash);
+        return convertedAddress;
+      }
+    };
+
+    ipn.recipientArray = recipientArray;
+    ipn.ipAddress = ipAddress;
+    // validate that transaction settles new order
+    invoiceDB.find({ invoice: req.body.invoice, custom: req.body.custom }, function (err, docs) {
+      if (err) {
+        // Error message if the paymentID doesn't match
+        console.log("Error fetching data from the database: ", err);
+      } else {
+        paidDB.insert(ipn)
+      }
+    });
+
+    // Send a response
+    res.send("OK");
+  }
+}
+
+app.post("/ipn", postIpn);
---
+++ Frontend/js/script.js
@@ -0,0 +1,174 @@
+const loginForm = document.getElementById('login-form');
+const signupForm = document.getElementById('signup-form');
+const depositForm = document.getElementById('deposit-form');
+document.getElementById('casino-loggedin').style.display = 'none';
+document.getElementById('casino-login').style.display = 'flex';
+document.getElementById('casino-signup-hero').style.display = 'block';
+const submitButton = document.getElementById('signup-submit');
+submitButton.style.display = 'none';
+const nextButton = document.getElementById('signup-next');
+const backButton = document.getElementById('signup-back');
+let signUpSlide = 1;
+console.log(depositForm)
+let user;
+
+nextButton.addEventListener('click', () => {
+  signUpSlide++;
+  if (signUpSlide === 3){
+    submitButton.style.display = 'block';
+  }
+})
+
+backButton.addEventListener('click', () => {
+  signUpSlide--;
+  if (signUpSlide < 3){
+    submitButton.style.display = 'none';
+  }
+})
+
+loginForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(loginForm);
+  const username = formData.get('username');
+  const password = formData.get('password');
+
+  const response = await fetch('/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password })
+  });
+
+  if (response.ok) {
+    const { token } = await response.json();
+
+    // Store the token in a cookie or local storage
+    document.cookie = `token=${token}; path=/`;
+
+    // Redirect to the home page
+    window.location.href = '/';
+  } else {
+    const { error } = await response.json();
+
+    // Display an error message to the user
+    const errorElement = document.querySelector('#login-error');
+    errorElement.textContent = error;
+  }
+});
+
+signupForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(signupForm);
+  const userData = {};
+
+  // Loop through the form elements and add them to the userData object
+  for (let element of formData) {
+    userData[element[0]] = element[1];
+  }
+
+  // Send the user data to the server
+  const response = await fetch('/signup', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userData)
+  });
+
+  if (response.ok) {
+    const { token } = await response.json();
+
+    // Store the token in a cookie or local storage
+    document.cookie = `token=${token}; path=/`;
+
+    // Redirect to the home page
+    window.location.href = '/';
+  } else {
+    const { error } = await response.json();
+
+    // Display an error message to the user
+    const errorElement = document.querySelector('#login-error');
+    errorElement.textContent = error;
+  }
+});
+
+depositForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const formData = new FormData(depositForm)
+  const amount = formData.get('amount');
+
+  const response = await fetch('/deposit', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ user, amount })
+  });
+  if (response.ok) {
+    const { payURL } = await response.json();
+    window.location.href = payURL;
+  } else {
+    const { error } = await response.json();
+    // Display an error message to the user
+    const errorElement = document.querySelector('#deposit-error');
+    errorElement.textContent = error;
+  }
+});
+
+
+
+// Get the token from a cookie or local storage
+const tokenCookie = document.cookie.split('; ').find(row => row.startsWith('token='));
+const token = tokenCookie ? tokenCookie.split('=')[1] : undefined;
+
+if (token) {
+  // Include the token in the request headers
+  const headers = { Authorization: `Bearer ${token}` };
+
+  // Make a request to a protected API endpoint
+  fetch('/api/protected', { headers })
+    .then(response => {
+      if (response.ok) {
+        // The token is valid, so display some protected data
+        return response.json();
+      } else {
+        // The token is invalid, so redirect to the login page
+        window.location.href = '/error';
+      }
+    })
+    .then(data => {
+      // Display the protected data to the user
+      console.log(data);
+      document.getElementById('casino-loggedin').style.display = 'flex';
+      document.getElementById('casino-login').style.display = 'none';
+      document.getElementById('casino-signup-hero').style.display = 'none';
+      document.getElementById('balance').innerHTML = data.user.mainBalance + data.user.bonusBalance;
+      document.getElementById('main-balance').innerHTML = data.user.mainBalance;
+      document.getElementById('bonus-balance').innerHTML = data.user.bonusBalance;
+      document.getElementById('username-ac').innerHTML = data.user.username;
+      document.getElementById('first-name-ac').innerHTML = data.user.firstname;
+      document.getElementById('last-name-ac').innerHTML = data.user.lastname;
+      document.getElementById('email-ac').innerHTML = data.user.email;
+      document.getElementById('address-ac').innerHTML = data.user.address;
+      document.getElementById('zipcode-ac').innerHTML = data.user.zipcode;
+      document.getElementById('county-ac').innerHTML = data.user.county;
+      document.getElementById('state-ac').innerHTML = data.user.state;
+      user = data.user.username;
+    })
+    .catch(error => {
+      // Handle any errors that occur during the request
+      console.error(error);
+    });
+} else {
+  // The token is not present, so redirect to the login page
+  //window.location.href = '/login';
+}
+
+// Get the logout button element
+const logoutButton = document.getElementById('logout');
+
+// Add a click event listener to the button
+logoutButton.addEventListener('click', function () {
+  // Remove the "token" cookie
+  document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+  // Redirect the user to the login page
+  window.location.href = '/';
+});
