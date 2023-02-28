0e5a2786bcfbd7c72770237831c31c601b2156db
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-02-28 13:59:34 +0000

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Start the server
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
